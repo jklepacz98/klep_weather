3c837dcbcf38cd09af664642110615974851c638
jklepacz <jklepacz@gsd.pl>
2022-12-30 15:28:23 +0100

    Added SnackBar message

--- lib/start/view/start_page.dart
+++ lib/start/view/start_page.dart
@@ -15,37 +15,58 @@ class StartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        //todo change or remove color
-        backgroundColor: Colors.amber,
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                  margin: const EdgeInsets.all(16.0),
-                  child: BlocProvider(
-                    create: (context) => getIt<WeatherBloc>(),
-                    child: BlocBuilder<WeatherBloc, WeatherState>(
-                      buildWhen: (previous, current) => previous != current,
-                      builder: (context, state) {
-                        return TextField(
-                          onSubmitted: (city) => context
-                              .read<WeatherBloc>()
-                              .add(WeatherLoadEvent(city: city)),
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'City',
-                          ),
-                        );
-                      },
-                    ),
-                  )),
-              Container(
-                margin: const EdgeInsets.all(16.0),
-                child: WeatherList(),
-              )
-            ],
-          ),
-        ));
+      appBar: AppBar(),
+      //todo change or remove color
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(16.0),
+              child: BlocProvider(
+                create: (context) => getIt<WeatherBloc>(),
+                child: BlocListener<WeatherBloc, WeatherState>(
+                  listener: (context, state) {
+                    if (state.status == WeatherStatus.success) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          backgroundColor: Colors.green,
+                          content: Text('Success'),
+                        ),
+                      );
+                    } else if (state.status == WeatherStatus.failure) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          backgroundColor: Colors.red,
+                          content: Text('Failure'),
+                        ),
+                      );
+                    }
+                  },
+                  child: BlocBuilder<WeatherBloc, WeatherState>(
+                    buildWhen: (previous, current) => previous != current,
+                    builder: (context, state) {
+                      return TextField(
+                        onSubmitted: (city) => context
+                            .read<WeatherBloc>()
+                            .add(WeatherLoadEvent(city: city)),
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'City',
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(16.0),
+              child: WeatherList(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
