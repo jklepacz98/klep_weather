fb1e382e14a7f57595c75f7b28dbd821d3ec98d5
jklepacz <jklepacz@gsd.pl>
2023-03-22 10:20:21 +0100

    Removed comments

--- lib/main/bloc/main_event.dart
+++ lib/main/bloc/main_event.dart
@@ -2,14 +2,6 @@ part of 'main_bloc.dart';
 
 @freezed
 abstract class MainEvent with _$MainEvent {
-  const factory MainEvent.localeChanged({required Locale locale}) = LocaleChangedEvent;
+  const factory MainEvent.localeChanged({required Locale locale}) =
+      LocaleChangedEvent;
 }
-
-// class LocaleChangedEvent extends MainEvent {
-//   const LocaleChangedEvent({this.locale});
-//
-//   final Locale? locale;
-//
-//   @override
-//   List<Object?> get props => [locale];
-// }
